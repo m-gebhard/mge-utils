e69bc0d05b840178556863a08253258c778da5c9
Marius Gebhard <marius.wspi@gmail.com>
2026-03-19 17:46:31 +0100

    Add separator as sibling in hierachy

--- Editor/Tools/EditorSeparatorAdder.cs
+++ Editor/Tools/EditorSeparatorAdder.cs
@@ -8,14 +8,22 @@ namespace MGeLabs.Utils.Editor
     {
         /// <summary>
         /// Adds an empty GameObject to act as a separator in the Unity hierarchy.
+        /// If an object is selected, the separator will be added as a sibling below the selected object; otherwise, it will be added at the root level.
         /// </summary>
         [MenuItem("GameObject/Separator", false, -10)]
         private static void AddSeparatorToHierarchy()
         {
             GameObject separator = new("----------------------------------");
             separator.SetActive(false);
-            Undo.RegisterCreatedObjectUndo(separator, "Add Separator");
 
+            GameObject selected = Selection.activeGameObject;
+            if (selected != null)
+            {
+                separator.transform.SetParent(selected.transform.parent, false);
+                separator.transform.SetSiblingIndex(selected.transform.GetSiblingIndex() + 1);
+            }
+
+            Undo.RegisterCreatedObjectUndo(separator, "Add Separator");
             Selection.activeGameObject = separator;
         }
     }
